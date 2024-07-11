df41cf2f7b9d20519ac11074d67549481f35e26a
Michael Bestas <mkbestas@lineageos.org>
2024-07-11 11:27:25 +0200

    cepheus: Include lineage FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,8 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/c2_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common//compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_cepheus
