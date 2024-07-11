45302162e4ae3b5651a80c7ffea6529cdeb2dece
Bruno Martins <bgcngm@gmail.com>
2024-07-11 11:23:23 +0200

    cepheus: Inherit from QTI FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,9 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/c2_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common//compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_cepheus
