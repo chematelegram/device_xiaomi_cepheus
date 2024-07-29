66331d7e7253654a6247963eb50a144f056da6b2
chematelegram <chfondia@gmail.com>
2024-07-29 08:34:33 +0200

    Revert "cepheus: Switch to common Xiaomi light AIDL"
    This reverts commit ae1e80ddccf74c1093fb5449f87775edc7a2a065.

--- device.mk
+++ device.mk
@@ -280,8 +280,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
-	
+    android.hardware.lights-service.cepheus
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
