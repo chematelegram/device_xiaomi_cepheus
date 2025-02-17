89ce31bbc37a490ce229632ce853150e3fa34a8d
chematelegram <chfondia@gmail.com>
2025-02-17 01:23:48 +0100

    cepheus: Revert rename prebuilt audio HAL and libs

--- extract-files.sh
+++ extract-files.sh
@@ -11,10 +11,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
-        vendor/lib/hw/audio.primary.cepheus.so)
-            [ "$2" = "" ] && return 0
-            sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_cepheus\.so\x00\x00\x00\x00|g" "${2}"
-            ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
             [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
